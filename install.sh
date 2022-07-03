facf3995e0e56eeda5053f34a6b3d5ca7e8f1ddb
Riki Nakamura <riki.nakamura.edu@gmail.com>
2022-07-03 18:49:57 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd $(dirname $0)/singularity
+
+if [ ! -f build_environment.sif ]; then
+    echo -e '\e[1;36mBuilding build_environment ...\e[m'
+    singularity build --fakeroot build_environment.sif build_environment.def
+    if [ $? -ne 0 ]; then
+        echo -e $'\e[1;31mFailed to build build_environment.\e[m'
+        exit 1
+    fi
+else
+    echo -e '\e[1;36mbuild_environment has already built.\e[m'
+fi
+
+echo -e '\e[1;36mBuilding executable ...\e[m'
+singularity build --fakeroot executable.sif executable.def
+if [ $? -ne 0 ]; then
+    echo -e $'\e[1;31mFailed to build executable.\e[m'
+    exit 1
+fi
+
+echo -e '\e[1;36mInstalling SMTPlan+ ...\e[m'
+sudo cp executable.sif /usr/local/bin/SMTPlan
+if [ $? -ne 0 ]; then
+    echo -e $'\e[1;31mFailed to install.\e[m'
+    exit 1
+fi
+echo -e '\e[1;36mInstalled SMTPlan+ successfully.\e[m'

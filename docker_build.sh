83f3f0e02469cf3f3caa19c4cb97dcc873f3ea84
Riki Nakamura <riki.nakamura.edu@gmail.com>
2022-10-18 16:58:08 +0900

    Add Dockerfiles

---
+++ docker_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+if [ ! "$(docker image ls -q "smtplan:build_environment")" ]; then
+    echo -e '\e[1;36mBuilding build_environment ...\e[m'
+    docker build --file=docker/Dockerfile.build_environment -t smtplan:build_environment .
+    if [ $? -ne 0 ]; then
+        echo -e $'\e[1;31mFailed to build build_environment.\e[m'
+        exit 1
+    fi
+else
+    echo -e '\e[1;36mbuild_environment has already built.\e[m'
+fi
+
+echo -e '\e[1;36mBuilding executable ...\e[m'
+docker build --rm --file=docker/Dockerfile -t smtplan .
+if [ $? -ne 0 ]; then
+    echo -e $'\e[1;31mFailed to build executable.\e[m'
+    exit 1
+fi
+
+echo -e '\e[1;36mBuild a SMTPlan+ Docker image successfully.\e[m'
